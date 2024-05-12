58a73f951f8c712d8ce3c18d03642a3329bf97d9
Tim <tim.grethler@gmail.com>
2024-05-12 15:04:50 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,67 +1,54 @@
 package main
 
 import (
-    "encoding/xml"
-    "fmt"
-    "io/ioutil"
-    "net/http"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-type Flussbad struct {
-    Name         string `xml:"title"`
-    Capacity     string `xml:"capacity"`
-    Status       string `xml:"status"`
-    LastModified string `xml:"lastModified"`
-    URL          string `xml:"url"`
+type BathInfo struct {
+	Title           string `xml:"title"`
+	TemperatureWater string `xml:"temperatureWater"`
+	OpenClosedText  string `xml:"openClosedTextPlain"`
+	URLPage         string `xml:"urlPage"`
 }
 
-type FlussbadData struct {
-    XMLName xml.Name   `xml:"flussbadData"`
-    Flussbads []Flussbad `xml:"flussbad"`
+type BathInfos struct {
+	XMLName xml.Name   `xml:"bathinfos"`
+	Baths   []BathInfo `xml:"baths>bath"`
 }
 
 func main() {
-    // Define a handler function
-    handler := func(w http.ResponseWriter, r *http.Request) {
-        // Fetch data from XML API
-        resp, err := http.Get("https://data.stadt-zuerich.ch/dataset/wassertemperaturen-freibaeder/resource/548d1ceb-1daf-4cf9-a14a-92c86326824d")
-        if err != nil {
-            http.Error(w, "Failed to fetch data", http.StatusInternalServerError)
-            return
-        }
-        defer resp.Body.Close()
+	// Make a GET request to the URL
+	resp, err := http.Get("https://www.stadt-zuerich.ch/stzh/bathdatadownload")
+	if err != nil {
+		fmt.Println("Failed to fetch data:", err)
+		return
+	}
+	defer resp.Body.Close()
 
-        // Read response body
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            http.Error(w, "Failed to read response body", http.StatusInternalServerError)
-            return
-        }
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Failed to read response body:", err)
+		return
+	}
 
-        // Parse XML
-       // Parse XML
-        var data FlussbadData
-        if err := xml.Unmarshal(body, &data); err != nil {
-            http.Error(w, fmt.Sprintf("Failed to parse XML: %v", err), http.StatusInternalServerError)
-            return
-        }
+	// Parse XML data
+	var bathInfos BathInfos
+	err = xml.Unmarshal(body, &bathInfos)
+	if err != nil {
+		fmt.Println("Failed to parse XML:", err)
+		return
+	}
 
-
-        // Display data in the response
-        for _, flussbad := range data.Flussbads {
-            fmt.Fprintf(w, "Name: %s\n", flussbad.Name)
-            fmt.Fprintf(w, "Capacity: %s\n", flussbad.Capacity)
-            fmt.Fprintf(w, "Status: %s\n", flussbad.Status)
-            fmt.Fprintf(w, "Last Modified: %s\n", flussbad.LastModified)
-            fmt.Fprintf(w, "URL: %s\n", flussbad.URL)
-            fmt.Fprintln(w, "------")
-        }
-    }
-
-    // Register the handler function to handle all requests to the root URL ("/")
-    http.HandleFunc("/", handler)
-
-    // Start the HTTP server on port 8080
-    fmt.Println("Server listening on :8080")
-    http.ListenAndServe(":8080", nil)
+	// Print bath information
+	for _, bath := range bathInfos.Baths {
+		fmt.Println("Title:", bath.Title)
+		fmt.Println("Temperature of water:", bath.TemperatureWater)
+		fmt.Println("Status (open/closed):", bath.OpenClosedText)
+		fmt.Println("URL:", bath.URLPage)
+		fmt.Println()
+	}
 }
